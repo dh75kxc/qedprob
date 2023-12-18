6b2a4a8fbbad36ce0dc8980ec13d73fe64837632
NewNyon <147021381+dh75kxc@users.noreply.github.com>
2023-12-18 14:38:49 +0900

    dd

--- main/js/index.js
+++ main/js/index.js
@@ -18,20 +18,20 @@ document.getElementById('test_btn6').addEventListener('click', function() {
 });
 
 document.getElementById('test_btn1').addEventListener('click', () => {
-  location.href = `main2.html?${0}`;
+  location.href = `main/main.html?${0}`;
 });
 document.getElementById('test_btn2').addEventListener('click', () => {
-    location.href = `main2.html?${1}`;
+    location.href = `main/main.html?${1}`;
   });
   document.getElementById('test_btn3').addEventListener('click', () => {
-    location.href = `main2.html?${2}`;
+    location.href = `main/main.html?${2}`;
   });
   document.getElementById('test_btn4').addEventListener('click', () => {
-    location.href = `main2.html?${3}`;
+    location.href = `main/main.html?${3}`;
   });
   document.getElementById('test_btn5').addEventListener('click', () => {
-    location.href = `main2.html?${4}`;
+    location.href = `main/main.html?${4}`;
   });
   document.getElementById('test_btn6').addEventListener('click', () => {
-    location.href = `main2.html?${5}`;
+    location.href = `main/main.html?${5}`;
   });
--- main/js/main.js
+++ main/js/main.js
@@ -1,14 +1,14 @@
 const receivedData = location.href.split('?')[1];
 
 document.getElementById('test_btn1').addEventListener('click', () => {
-    location.href = `main3.html?${receivedData}${0}`;
+    location.href = `main2.html?${receivedData}${0}`;
 });
 document.getElementById('test_btn2').addEventListener('click', () => {
-    location.href = `main3.html?${receivedData}${1}`;
+    location.href = `main2.html?${receivedData}${1}`;
 });
 document.getElementById('test_btn3').addEventListener('click', () => {
-    location.href = `main3.html?${receivedData}${2}`;
+    location.href = `main2.html?${receivedData}${2}`;
 });
 document.getElementById('test_btn4').addEventListener('click', () => {
-    location.href = `main3.html?${receivedData}${3}`;
+    location.href = `main2.html?${receivedData}${3}`;
 });
