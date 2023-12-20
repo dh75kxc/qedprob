5ac91b1c1d512f21f0dabc6f99b9815a7ab2a27c
NewNyon <147021381+dh75kxc@users.noreply.github.com>
2023-12-20 18:04:57 +0900

    dd

--- main/js/main3.js
+++ main/js/main3.js
@@ -1,8 +1,13 @@
 const receivedData = location.href.split('?')[1];
 if (receivedData == 32384029384) {
     document.querySelector('#res').innerText = "정답입니다!!";
+    document.querySelector('#outer b').setAttribute('style', 'color: hotpink;');
 } else if (receivedData == 20110523234) {
     document.querySelector('#res').innerText = "오답입니다.";
+    document.querySelector('#outer b').setAttribute('style', 'color: darkviolet;');
 } else {
     document.querySelector('#res').innerText = "더러운 해커자식";
 }
+setTimeout(function() {
+    location.href = `../index.html`;
+}, 8000);
