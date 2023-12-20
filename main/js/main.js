a20acbef386ffc323c7e83691755a97961d6d465
NewNyon <147021381+dh75kxc@users.noreply.github.com>
2023-12-20 10:54:41 +0900

    dd

--- main/js/main.js
+++ main/js/main.js
@@ -1,5 +1,17 @@
 const receivedData = location.href.split('?')[1];
 
+let image = [[['001','002'],['001','002','003','004'],['001','002','003','004'],['001','002']],
+            [['001','002'],['001','002','003','004','005'],['001','002','003','004','005'],['001']],
+            [[],[],[],[]],
+            [[],[],[],[]],
+            [[],[],[],[]],
+            [[],[],[],[]]];
+
+document.querySelector('#probnum1').innerText = image[receivedData][0].length+" Problems";
+document.querySelector('#probnum2').innerText = image[receivedData][1].length+" Problems";
+document.querySelector('#probnum3').innerText = image[receivedData][2].length+" Problems";
+document.querySelector('#probnum4').innerText = image[receivedData][3].length+" Problems";
+
 document.getElementById('test_btn1').addEventListener('click', () => {
     location.href = `main2.html?${receivedData},${0}`;
 });
